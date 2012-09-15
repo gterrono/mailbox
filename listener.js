a84c0ba6df4ab158ca621f22977ac0f51ad2a2bf
Greg Terrono <gterrono@seas.upenn.edu>
2012-09-15 17:46:40 -0400

    Logs when you are in an email and when it has an attachment

--- listener.js
+++ listener.js
@@ -1,6 +1,17 @@
-alert("hello");
+console.log("Listener loaded");
 
 setTimeout(function() {
-  console.log($('#canvas_frame').contents().find('tr.zA'));
-  $('#canvas_frame').contents().find('tr.zA').click(function(){alert('you have mail')});
-}, 3000);
+  function checkIsMail(){
+  if($('#canvas_frame').contents().find('span.hP').length > 0) {
+    console.log('you are looking at mail');
+    if($('#canvas_frame').contents().find('div.hq.gt').length > 0) {
+      console.log('you have an attachment');
+    }
+    return;
+  }
+  else {
+    setTimeout(checkIsMail, 2000);
+  }
+  }
+  checkIsMail();
+}, 6000);
