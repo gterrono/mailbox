57ce1068d53dafb10a9f72bb861a0d8b05326bf5
Greg Terrono <gterrono@seas.upenn.edu>
2012-09-15 16:48:57 -0400

    Did stuff

--- content_script.js
+++ content_script.js
@@ -3,7 +3,7 @@ var referrer = document.referrer;
 if (_.isString(referrer)) {
   var matches = referrer.match(/oauth_consumer_key=([^&]+)/);
 
-  if (matches.length > 0) {
+  if (matches && matches.length > 0) {
     if  (RegExp.$1 === OAUTH_CONSUMER_KEY) {
       chrome.extension.sendRequest({ "event": "sign" });
 
--- listener.js
+++ listener.js
@@ -1,1 +1,6 @@
 alert("hello");
+
+setTimeout(function() {
+  console.log($('#canvas_frame').contents().find('tr.zA'));
+  $('#canvas_frame').contents().find('tr.zA').click(function(){alert('you have mail')});
+}, 3000);
