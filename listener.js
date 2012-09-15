6b8ce2564638e18e02d522180c49fbb896ce513a
Greg Terrono <gterrono@seas.upenn.edu>
2012-09-15 19:40:26 -0400

    Can get a file from a url

--- listener.js
+++ listener.js
@@ -4,8 +4,15 @@ setTimeout(function() {
   function checkIsMail(){
   if($('#canvas_frame').contents().find('span.hP').length > 0) {
     console.log('you are looking at mail');
-    if($('#canvas_frame').contents().find('div.hq.gt').length > 0) {
+    var $attachments = $('#canvas_frame').contents().find('div.hq.gt');
+    if($attachments.length > 0) {
       console.log('you have an attachment');
+      var url = $attachments.find('span > a.e').attr('href');
+      console.log(url);
+      $.get(url, function(response, status, xhr) {
+        console.log(response);
+        console.log("xhr", xhr);
+      }).error(function(e, s){console.log('error', e.status);});
     }
     return;
   }
